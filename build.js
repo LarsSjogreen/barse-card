9929d816ee4666ef0d2f5a95f375d0bb32baccf2
larssjogreen <lars.sjogreen@akvo.org>
2019-03-13 15:41:09 +0100

    * Changed title a bit * Added another web page

--- build.js
+++ build.js
@@ -17,7 +17,7 @@ const options = {
 const data = {
   name: chalk.white('               Lars Sjögreen'),
   handle: chalk.white('larssjogreen, barse'),
-  work: chalk.white('Consultant Detective, Jack of all trades'),
+  work: chalk.white('Consulting Detective, Jack of all trades'),
   twitter: chalk.gray('https://twitter.com/') + chalk.cyan('barse'),
   npm: chalk.gray('https://npmjs.com/') + chalk.red('~barse'),
   github: chalk.gray('https://github.com/') + chalk.green('larssjogreen'),
@@ -25,6 +25,7 @@ const data = {
   linkedin: chalk.gray('https://linkedin.com/in/') + chalk.yellow('larssjogreen'),
   bgg: chalk.gray('https://boardgamegeek.com/collection/user/') + chalk.green('barse'),
   web: chalk.cyan('https://labs.sjögreen.se/'),
+  web2: chalk.cyan('https://kodapa.nu'),
   npx: chalk.red('npx') + ' ' + chalk.white('@barse/card'),
   labelWork: chalk.white.bold('       Work:'),
   labelTwitter: chalk.white.bold('    Twitter:'),
@@ -46,6 +47,7 @@ const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const bgging = `${data.labelBGG} ${data.bgg}`
 const webing = `${data.labelWeb}  ${data.web}`
+const webing2 = `${data.labelWeb}  ${data.web2}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
@@ -57,6 +59,7 @@ const output = heading + // data.name + data.handle
                githubing + newline + // data.labelGitHub + data.github
                linkedining + newline + // data.labelLinkedIn + data.linkedin
                bgging + newline +
+               webing2 + newline +
                webing + newline + newline + // data.labelWeb + data.web
                carding // data.labelCard + data.npx
 
